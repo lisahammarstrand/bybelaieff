b64c2159d20f9418c8e2e98795eb89edc0973f40
Lisa Hammarstrand <lisa@Lisas-MacBook-Air.local>
2021-04-30 10:33:53 +0200

    404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,23 @@
 import React from "react"
+import styled from 'styled-components'
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+const PageNotFound = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: #17525F;
+  padding: 100px;
+`
+
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <PageNotFound>
+      <h1>404: Not found</h1>
+      <p>Den här sidan finns inte. Gå tillbaka och försök igen!</p>
+    </PageNotFound>
   </Layout>
 )
 
